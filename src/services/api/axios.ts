a748cbe04677413a8915738b8cb33d71878cdc47
daraselia <zdaras@brokers.io>
2022-06-15 17:08:42 +0400

    axios interceptors

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import storage from '@/utils/storage';
 import { ILoginResponse } from '@/types/models/user';
@@ -8,12 +8,9 @@ import { store } from '@/store';
 import config from '@/utils/config';
 
 export const otpHeader = 'x-as-otp';
-
-const baseURL = config.API_BASE_URL;
 export const authToken = String(config.AUTH_TOKEN);
-
+const baseURL = config.API_BASE_URL;
 const instance = axios.create({ baseURL });
-
 const noAuthInstance = axios.create({ baseURL });
 
 export const setAuthHeader = (response: Pick<ILoginResponse, 'access_token' | 'refresh_token'>) => {
@@ -43,37 +40,50 @@ function getAuthToken(refreshToken: string) {
 	return authTokenRequest;
 }
 
-instance.interceptors.response.use(
-	res => res,
-	async (error: AxiosError<IError>) => {
-		const originalRequest: AxiosRequestConfig & { _retry?: boolean } = error.config;
-		const invalidToken = error.response?.status === 401;
-
-		if (invalidToken) {
-			const refreshToken = storage('refresh_token').get();
-
-			if (!['undefined', 'null'].includes(String(refreshToken)) && !originalRequest._retry) {
-				delete originalRequest.headers?.[authToken];
-				try {
-					const res = await getAuthToken(refreshToken);
-					if ([200, 201].includes(res.status)) {
-						setAuthHeader(res.data);
-						originalRequest._retry = true;
-						if (originalRequest.headers) originalRequest.headers[authToken] = `Bearer ${res.data.access_token}`;
-						return instance(originalRequest);
-					}
-				} catch (err: any) {
-					store.dispatch<any>(userActions.logout());
-					return Promise.reject(err?.response?.data);
-				}
-			} else {
-				return Promise.reject(error?.response?.data);
-			}
-		}
-
-		return Promise.reject(error?.response?.data);
+const requestInterceptor = (cfg: AxiosRequestConfig) => {
+	if (cfg.data && !(cfg.data instanceof FormData) && !(cfg.data instanceof URLSearchParams)) {
+		const { otp, ...data } = cfg.data;
+		cfg.data = data;
+		if (![undefined, null].includes(otp) && cfg.headers) cfg.headers[otpHeader] = otp; // transfer 2fa code from params to header
 	}
-);
+
+	return cfg;
+};
+
+const responseSuccessInterceptor = (response: AxiosResponse): AxiosResponse => response;
+
+const responseErrorInterceptor = async (error: AxiosError<IError>) => {
+	const originalRequest: AxiosRequestConfig & { _retry?: boolean } = error.config;
+	const invalidToken = error.response?.status === 401;
+
+	if (invalidToken) {
+		const refreshToken = storage('refresh_token').get();
+
+		if (!['undefined', 'null'].includes(String(refreshToken)) && !originalRequest._retry) {
+			delete originalRequest.headers?.[authToken];
+			try {
+				const res = await getAuthToken(refreshToken);
+				if ([200, 201].includes(res.status)) {
+					setAuthHeader(res.data);
+					originalRequest._retry = true;
+					if (originalRequest.headers) originalRequest.headers[authToken] = `Bearer ${res.data.access_token}`;
+					return instance(originalRequest);
+				}
+			} catch (err: any) {
+				store.dispatch<any>(userActions.logout());
+				return Promise.reject(err?.response?.data);
+			}
+		} else {
+			return Promise.reject(error?.response?.data);
+		}
+	}
+
+	return Promise.reject(error?.response?.data);
+};
+
+instance.interceptors.request.use(requestInterceptor, err => Promise.reject(err));
+
+instance.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor);
 
 export const { get, post, put, delete: del } = instance;
 
