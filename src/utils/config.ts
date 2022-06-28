cf30cda09455b5114a5ee09e8fe9ed802d8fc3ce
daraselia <zdaras@brokers.io>
2022-06-28 18:48:05 +0400

    ..

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,7 @@ export default {
 	PORT: import.meta.env.VITE_PORT,
 	API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
 	AUTH_TOKEN: import.meta.env.VITE_AUTH_TOKEN,
-	isDev: import.meta.env.DEV,
-	isProd: import.meta.env.PROD
+	isDev: import.meta.env.VITE_BUILD_MODE === 'development' || !import.meta.env.VITE_BUILD_MODE,
+	isTestBuild: import.meta.env.VITE_BUILD_MODE === 'test',
+	isProd: import.meta.env.VITE_BUILD_MODE === 'production'
 };
